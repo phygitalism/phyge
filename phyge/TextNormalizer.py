d6b7ccca883f3f65edddcb2d15d469f213da45be
Sergey Polischouck <polischouckserg@gmail.com>
2018-05-11 19:10:39 +0300

    use our stopwords and get rid of nltk

--- phyge/TextNormalizer.py
+++ phyge/TextNormalizer.py
@@ -1,8 +1,6 @@
 import re
 import pymorphy2
 
-from nltk.corpus import stopwords
-
 
 class TextNormalizer:
 
@@ -11,7 +9,8 @@ class TextNormalizer:
         text_tokens = cls.__tokenize_text(text)
         russian_words = re.compile('[А-Яа-я]+').findall(' '.join(text_tokens))
         russian_words = cls.__normalize_tokens(russian_words)
-        return list(filter(lambda x: x not in stopwords.words('russian') and len(x) > 1, russian_words))
+        stopwords = cls.stopwords()
+        return list(filter(lambda x: x not in stopwords and len(x) > 1, russian_words))
 
     @classmethod
     def __tokenize_text(cls, text):
@@ -22,3 +21,9 @@ class TextNormalizer:
     def __normalize_tokens(cls, tokens):
         morph = pymorphy2.MorphAnalyzer()
         return list(map(lambda x: morph.parse(x)[0].normal_form, tokens))
+
+    @classmethod
+    def stopwords(cls):
+        with open('Resources/stopwords/russian', 'r', encoding='utf8') as file:
+            lines = file.readlines()
+            return [line.replace('\n', '') for line in lines]
