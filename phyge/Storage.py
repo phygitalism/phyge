c1955bbf58ce0386324edf28a35fed670e82745c
Екатерина Симонова <kate.sim@maas.agency>
2018-05-15 15:41:59 +0300

    add /tmp/

--- phyge/Storage.py
+++ phyge/Storage.py
@@ -12,12 +12,15 @@ from Models.Query import Query
 class Storage:
     def load_test_case(self, test_case_id):
         test_case_path = str.format('{0}/test_{1}', PhyVariables.testsPath, test_case_id)
+        tmp_path = (test_case_path + '/tmp/')
+        if not os.path.exists(tmp_path):
+            os.makedirs(tmp_path)
+
         urls = []
         with open(test_case_path + '/' + PhyVariables.urlsFileKey, 'r', encoding="utf8") as json_file:
             data_urls = json.load(json_file)
         for article in data_urls:
             urls.append(article.get('url'))
-
 
         # data_url = pd.read_csv(test_case_path + '/' + PhyVariables.urlsFileKey)
         # urls = data_url.iloc[:, 0]
@@ -25,11 +28,11 @@ class Storage:
 
         saved_articles, values = list(), None
 
-        if os.path.isfile(test_case_path + '/' + PhyVariables.articlesFileKey):
-            saved_articles = ArticleSerializer.deserialize(test_case_path + '/' + PhyVariables.articlesFileKey)
+        if os.path.isfile(tmp_path + PhyVariables.articlesFileKey):
+            saved_articles = ArticleSerializer.deserialize(tmp_path + PhyVariables.articlesFileKey)
 
-        if os.path.isfile(test_case_path + '/' + PhyVariables.valuesFileKey):
-            values = pd.read_csv(test_case_path + '/' + PhyVariables.valuesFileKey, dtype='unicode')
+        if os.path.isfile(tmp_path + PhyVariables.valuesFileKey):
+            values = pd.read_csv(tmp_path + PhyVariables.valuesFileKey, dtype='unicode')
 
         return TestCase(test_case_id, {'urls': urls,
                                        'path': test_case_path,
