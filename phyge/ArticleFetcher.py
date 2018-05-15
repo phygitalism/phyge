698cfc5cebd70c2c1c121f92c70f00fa62c6e5c8
Екатерина Симонова <kate.sim@maas.agency>
2018-05-15 16:11:00 +0300

    fix empty article.normalize_words

--- phyge/ArticleFetcher.py
+++ phyge/ArticleFetcher.py
@@ -1,4 +1,3 @@
-
 from Models.PhygeArticle import PhyArticle
 from newspaper import Article
 
@@ -14,10 +13,13 @@ class ArticleFetcher:
             article_html = Article(url=current_url, language='ru')
             article_html.download()
             if len(article_html.html) > 0:
-            # article_html = request(url=current_url, method='GET').text
+                # article_html = request(url=current_url, method='GET').text
                 article = PhyArticle()
                 article.transform(article_html, current_url)
-                self.articles.append(article)
+                if len(article.normalized_words) > 0:
+                    self.articles.append(article)
+                else:
+                    print('NO FOUND words in the article', number, 'url:', current_url)
             else:
                 print('article', number, 'doesn\'t parsed! Url:', current_url)
         return self.articles
