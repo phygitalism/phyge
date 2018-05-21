0b7f72e4f8f15074c3ceceee53941fc8dad7d19d
Екатерина Симонова <kate.sim@maas.agency>
2018-05-21 15:44:27 +0300

    add curruntTestKey

--- phyge/Models/PhygeVariables.py
+++ phyge/Models/PhygeVariables.py
@@ -1,8 +1,9 @@
 
 class PhyVariables:
     testsPath = 'Tests'
-
+    currentTestKey = 4
     urlsFileKey = 'urls.json'
+    # urlsFileKey = 'en_slack_urls_clear.json'
     articlesFileKey = 'articles.json'
     queriesFileKey = 'queries.json'
     valuesFileKey = 'values.csv'
