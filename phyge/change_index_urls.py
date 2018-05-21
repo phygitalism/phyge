d936a99f8c5e23da5733762a15e69041a53a5e76
Екатерина Симонова <kate.sim@maas.agency>
2018-05-21 14:26:18 +0300

    create urls in test_6 how sum(en+ru urls in resourses)

--- phyge/change_index_urls.py
+++ phyge/change_index_urls.py
@@ -1,13 +1,19 @@
 import json
 from pprint import pprint
 
-with open('Tests/test_6/urls.json', 'r', encoding="utf8") as json_file:
-    urls = json.load(json_file)
-pprint(urls)
 
-for index, articles in enumerate(urls):
+with open('Resources/en_slack_urls_clear.json', 'r', encoding="utf8") as json_file:
+    en_urls_clear = json.load(json_file)
+
+with open('Resources/ru_slack_urls_clear.json', 'r', encoding="utf8") as json_file:
+    ru_urls_clear = json.load(json_file)
+
+all_urls = en_urls_clear + ru_urls_clear
+
+
+for index, articles in enumerate(all_urls):
     articles['id'] = index
-pprint(urls)
+pprint(all_urls)
 
 with open('Tests/test_6/urls.json', 'w+', encoding="utf8") as file:
-    json.dump(urls, file, indent=2, ensure_ascii=False)
+    json.dump(all_urls, file, indent=2, ensure_ascii=False)
