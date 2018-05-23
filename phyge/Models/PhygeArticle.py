528ef3bdd1bfa6c9305c9c8ddcde81e68e065081
evgen <evgen19-97@ya.ru>
2018-05-23 18:14:33 +0300

    add translate out_info has topics that weren't translated well

--- phyge/Models/PhygeArticle.py
+++ phyge/Models/PhygeArticle.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 from TextNormalizer import TextNormalizer
 
+#from googletrans import Translator
 
 class PhyArticle:
     def __init__(self, obj=None):
@@ -28,9 +29,30 @@ class PhyArticle:
         self.readable_html = Document(article_html.html).summary()
         self.title = Document(article_html.html).short_title()
         self.text = self.__transform_to_single_line(self.readable_html)
+
+        #self.language = Translator().detect(self.text).lang
+        #print('language is', self.language)
+
         if self.language == 'en':
-            pass
+            try:
+                from googletrans import Translator
+                print('Translate')
+                if len(self.text) < 5000:
+                    self.text = Translator().translate(self.text, dest='ru').text
+                else:
+                    from math import ceil
+                    block_number = ceil(len(self.text) / 5000) # Определяем количество блоков, которые будем переводить
+                    translated_text = ''
+                    for i in range(block_number):
+                        translated_text += Translator().translate(self.text[5000*i:5000*(i+1)], dest='ru').text
+                    self.text = translated_text
+            except:
+                print('Error while translating')
+                with open('out_info.txt', 'a') as info:
+                    info.write("Cann't be translated " + self.title + "\n")
+
         self.normalized_words = TextNormalizer.normalize(self.text)
+
 
     def __transform_to_single_line(self, raw_html):
         soup = BeautifulSoup(raw_html, "lxml")
