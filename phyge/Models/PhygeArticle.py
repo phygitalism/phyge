79f30cc7f104cfb7e10f52c4d52121840ea44380
Екатерина Симонова <kate.sim@maas.agency>
2018-05-23 14:06:31 +0300

    translate text article

--- phyge/Models/PhygeArticle.py
+++ phyge/Models/PhygeArticle.py
@@ -14,6 +14,7 @@ class PhyArticle:
         self.readable_html = obj.get('readable_html', '')
         self.text = obj.get('text', '')
         self.normalized_words = obj.get('normalized_words', list())
+        self.language = obj.get('language', '')
 
     def deserialize_from(self, obj: dict):
         return PhyArticle(obj)
@@ -26,6 +27,8 @@ class PhyArticle:
         self.readable_html = Document(article_html.html).summary()
         self.title = Document(article_html.html).short_title()
         self.text = self.__transform_to_single_line(self.readable_html)
+        if self.language == 'en':
+            pass
         self.normalized_words = TextNormalizer.normalize(self.text)
 
     def __transform_to_single_line(self, raw_html):
@@ -36,4 +39,5 @@ class PhyArticle:
         return {'url': self.downloaded_from_url,
                 'title': self.title,
                 'text': self.text,
+                'language': self.language,
                 'normalized_words': self.normalized_words}
