e5365bad6291d03a8bd3cb33677fd2b8067027a5
Екатерина Симонова <kate.sim@maas.agency>
2018-07-10 10:49:30 +0300

    update response dict

--- phyge/PhydgeServer.py
+++ phyge/PhydgeServer.py
@@ -22,13 +22,14 @@ def search_articles():
     query_text = search_articles["query"]
     amount = search_articles["amount"]
     queries = [Query({'text': query_text, 'id': 1})]
-    search = SearchEngine(query=queries, test_case_id=4, model_name='w2v')
+    search = SearchEngine(query=queries, test_case_id=4, model_name='lda')
     if search.server_state == ServerState.Stop:
         print("\nCan't start server, model didn't loaded\n")
         search.train_model()
     else:
         print("\nStart server\n")
-        models_answer = search.get_answers(amount=amount)
+        answers = search.get_answers(amount=amount)
+        models_answer = {answers[0]['model_name']:answers[0]["answer_articles"]}
 
     return Response(json.dumps(models_answer, ensure_ascii=False), status=200, mimetype='application/json')
 
